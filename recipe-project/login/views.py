ddfb6bdb838983939f2c8d10794470366d6584d6
안정현 <dks0665@naver.com>
2019-12-16 04:01:54 +0900

    로그인 오류 수정

--- recipe-project/login/views.py
+++ recipe-project/login/views.py
@@ -8,7 +8,7 @@ def signup(request):
 			user = User.objects.create_user(
 				username=request.POST['username'],password=request.POST["password1"])
 			auth.login(request,user)
-			return redirect('home')
+			return redirect('homePage')
 		return render(request, 'signup.html')
 
 	return render(request, 'signup.html')
@@ -20,9 +20,9 @@ def login(request):
 		user = auth.authenticate(request, username=username, password=password)
 		if user is not None:
 			auth.login(request, user)
-			return redirect('home')
+			return redirect('homePage')
 		else:
-			return render(request, 'login.html', {'error': '아이디 혹은 비밀번호를 확인해주세요!'})
+			return render(request, 'login.html', {'error': 'check ID or PassWord!'})
 	else:
 		return render(request, 'login.html')
 
