3eed9968129074fa3c69b06fbb476dcd1eb4a93f
ajh3533 <ajh3533@naver.com>
2019-12-18 03:27:06 +0900

    공지사항 완성

--- recipe-project/notice/urls.py
+++ recipe-project/notice/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.main, name='noticeMain'),
-    # path('login/',views.login, name='login'),
-    # path('logout/',views.logout, name='logout')
+    path('notice1/',views.notice1, name='notice1'),
+    path('notice2/',views.notice2, name='notice2')
     ]
--- recipe-project/notice/views.py
+++ recipe-project/notice/views.py
@@ -6,6 +6,13 @@ from .models import Notice
 # Create your views here.
 
 def main(request):
-    # return render(request, 'home.html')
     mains = Notice.objects.all
     return render(request, 'main.html', {'mains':mains})
+
+def notice1(request):
+    notice1 = Notice.objects.all
+    return render(request, 'notice1.html', {'notice1':notice1})
+
+def notice2(request):
+    notice2 = Notice.objects.all
+    return render(request, 'notice2.html', {'notice2':notice2})
